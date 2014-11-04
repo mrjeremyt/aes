2fdbd6eca84be3435ec0969a4fec3f2573042622
mrjeremyt <jthompson2214@gmail.com>
2014-11-04 16:13:04 -0600

    decrypt is very close to done. Just need to debug a bit

--- src/AES.java
+++ src/AES.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 public class AES 
 {
 	protected static int [][] x_box;
+	protected static int [][] inv_x_box;
 	protected static int [][] state;
 	protected static int [][] key;
 	protected static int [][] expanded_key;
@@ -20,6 +21,7 @@ public class AES
 	public static void main(String[] args) throws IOException 
 	{
 		fill_xbox();
+		fill_inv_xbox();
 		fill_rcon();
 		fill_e();
 		fill_l();
@@ -50,7 +52,6 @@ public class AES
 		while (sc.hasNextLine())
 		{
 			make_a_state(sc);
-			make_mix_col_matrix();
 			int ex_key = 0;
 			ex_key = addRoundKey(ex_key);
 			
@@ -68,28 +69,52 @@ public class AES
 			subBytes();
 			shiftRows();
 			ex_key = addRoundKey(ex_key);
+			System.out.println(ex_key);
 //			print_array(state, true);
 			pw.println(string_from_state());
 		}
 	}
 
-	private static void make_mix_col_matrix() {
-		mix_col_matrix = new int[][]
-				{
-					{2, 3, 1, 1},
-					{1, 2, 3, 1},
-					{1, 1, 2, 3},
-					{3, 1, 1, 2}
-				};
-	}
-
 
 	static void decrypt(Scanner sc, PrintWriter pw){
-		while(sc.hasNextLine()){
+		while (sc.hasNextLine())
+		{
 			make_a_state(sc);
+			int ex_key = 43;
+			ex_key = (invAddRoundKey(ex_key));
+//			System.out.println("ex_key: " +ex_key);
+			invShiftRows();
+			invSubBytes();
+			
+			int round = 9;
+			while(round-- > 0){
+				ex_key = invAddRoundKey(ex_key);
+				invMixColumns();
+				invShiftRows();
+				invSubBytes();
+			}
+
+			ex_key = invAddRoundKey(ex_key);
+			pw.println(string_from_state());
 		}
 	}
 	
+	private static void invSubBytes() {
+		for(int i = 0; i < state.length; i++){
+			for(int j = 0; j < state[0].length; j++){
+				int top = (state[j][i] >> 4);
+				int bottom = (state[j][i] & 0x0F);
+//				System.out.println("top: " + top);
+//				System.out.println("bottom: " + bottom);
+//				System.out.println("i: " + i);
+//				System.out.println("j: " + j);
+//				System.out.println("state: " + state[j][i]);
+//				state[j][i] = inv_x_box[top][bottom];
+			}
+		}
+	}
+
+
 	static String string_from_state(){
 		StringBuilder sb = new StringBuilder();
 		for(int i = 0; i < state.length; i++){
@@ -148,32 +173,52 @@ public class AES
 //		print_array(state);
 	}
 	
-	static void mixColumns(){
-//		ArrayList<int[]> cols = new ArrayList<int[]>();
-//		for(int i = 0; i < state.length; i++){
-//			int[] state_col = new int[4];
-//			for(int j = 0; j < state[i].length; j++){
-//				state_col[j] = state[j][i];
-//			}
-//			cols.add(state_col);
-//		}
-////		print_array_1d(cols.get(0),true);
-////		print_array_1d(cols.get(1),true);
-////		print_array_1d(cols.get(2),true);
-////		print_array_1d(cols.get(3),true);
-////		System.out.println();
-//		
-//		for(int i = 0; i < cols.size(); i++){
-//			for(int j = 0; j < cols.get(i).length; j++){
-//				state[j][i] = matrix_multiply(cols.get(i), mix_col_matrix[i]);
-//			}
-//		}
+	static void invShiftRows(){
+		//shift of 1
+		int[] row_1 = state[1];
+		int[] row_2 = state[2];
+		int[] row_3 = state[3];
 		
+		//setup the temp vars
+		int temp_1;
+		int temp_2;
+		int temp_3;
+		
+//		print_array(state);
+		
+		//first shift
+		temp_1 = row_1[3];
+		row_1[0] = temp_1; row_1[1] = row_1[0]; row_1[2] = row_1[1]; row_1[3] = row_1[2];
+		
+		//second shift
+		temp_1 = row_2[3];
+		temp_2 = row_2[2];
+		row_2[0] = temp_2; row_2[1] = temp_1; row_2[2] = row_2[0]; row_2[3] = row_2[1];
+		
+		//third shift
+		temp_1 = row_3[1];
+		temp_2 = row_3[2];
+		temp_3 = row_3[3];
+		row_3[0] = temp_1; row_3[1] = temp_2; row_3[2] = temp_3; row_3[3] = row_3[0];
+		
+		//put back into state
+		state[1] = row_1;
+		state[2] = row_2;
+		state[3] = row_3;
+		
+//		print_array(state);
+	}
+	
+	private static void mixColumns(){		
 		for(int i = 0; i < 4; i++){
 			mixColumn2(i);
 		}
 	}
 	
+	private static void invMixColumns(){
+		for(int i = 0; i < 4; i++)
+			invMixColumn2(i);
+	}
 	
 ////////////////////////the mixColumns Tranformation ////////////////////////
 	private static int mul (int a, int b) {
@@ -214,7 +259,7 @@ public class AES
 		state[3][c] = (mul(2,a[3]) ^ a[1] ^ a[2] ^ mul(3,a[0]));
 	} // mixColumn2
 	
-	public void invMixColumn2 (int c) {
+	public static void invMixColumn2 (int c) {
 		int a[] = new int[4];
 		
 		//note that a is just a copy of st[.][c]
@@ -228,7 +273,7 @@ public class AES
 	} // invMixColumn2
 	
 	
-	static int addRoundKey(int round)
+	private static int addRoundKey(int round)
 	{
 		int[] state_col = new int[4];
 		int[] ek_col = new int[4];
@@ -238,6 +283,24 @@ public class AES
 				ek_col[j] = expanded_key[j][round];
 			}	
 			round++;
+			state_col = xor(state_col, ek_col);
+			for(int j = 0; j < state[i].length; j++){
+				state[j][i] = state_col[j];
+			}
+		}
+		return round;
+	}
+	
+	private static int invAddRoundKey(int round)
+	{
+		int[] state_col = new int[4];
+		int[] ek_col = new int[4];
+		for(int i = 0; i < state.length; i++){
+			for(int j = 0; j < state[i].length; j++){
+				state_col[j] = state[j][i];
+				ek_col[j] = expanded_key[j][round];
+			}	
+			round--;
 			state_col = xor(state_col, ek_col);
 			for(int j = 0; j < state[i].length; j++){
 				state[j][i] = state_col[j];
@@ -424,6 +487,30 @@ public class AES
 			   {0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E},
 			   {0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF},
 			   {0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16}
+			};
+	}
+	
+	
+	static void fill_inv_xbox ()
+	{
+		inv_x_box = new int [][]
+		{
+				{0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,},
+				{0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,},
+				{0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,},
+				{0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,},
+				{0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,},
+				{0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,},
+				{0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,},
+				{0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,},
+				{0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,},
+				{0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,},
+				{0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,},
+				{0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,},
+				{0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,},
+				{0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,},
+				{0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,},
+				{0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D,}
 			};
 	}
 	
